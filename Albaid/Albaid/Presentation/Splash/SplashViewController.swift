b6eabb187cbab747ba31b9f41a4892c0cd8c08bb
Bibi-urssu <bibi.urssu@gmail.com>
2024-09-27 15:42:17 +0900

    feat: SplashView UI 구현

--- Albaid/Albaid/Presentation/Splash/SplashViewController.swift
+++ Albaid/Albaid/Presentation/Splash/SplashViewController.swift
@@ -8,5 +8,24 @@
 import Foundation
 
 class SplashViewController: BaseViewController {
+
+    // MARK: UI Components
+    private let splashView = SplashView()
     
+    // MARK: Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: Configuration
+    override func configureSubviews() {
+        view.addSubview(splashView)
+    }
+
+    // MARK: Layout
+    override func makeConstraints() {
+        splashView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
 }
