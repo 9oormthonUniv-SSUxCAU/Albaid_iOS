84e9a6b3f9d3ba9d922af7072c4dcaa7542aedb2
Bibi-urssu <bibi.urssu@gmail.com>
2024-09-27 15:41:29 +0900

    add: UIColor+Extension
    main color, sub color

---
+++ Albaid/Albaid/Extension/UIColor+Extension.swift
@@ -0,0 +1,19 @@
+//
+//  UIColor+Extension.swift
+//  Albaid
+//
+//  Created by 박지윤 on 9/27/24.
+//
+
+import UIKit
+
+extension UIColor {
+
+    /// main color
+    static let mainGreen = UIColor(named: "green")
+
+    /// sub color
+    static let subYellow = UIColor(named: "yellow")
+    static let subBlue = UIColor(named: "blue")
+    static let subPink = UIColor(named: "pink")
+}
