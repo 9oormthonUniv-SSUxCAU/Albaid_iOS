a57173d38eea8e5961187ac5bc2884a598ca3d3c
Bibi-urssu <bibi.urssu@gmail.com>
2024-09-28 02:26:30 +0900

    add: UIColor+Extension

--- Albaid/Albaid/Extension/UIColor+Extension.swift
+++ Albaid/Albaid/Extension/UIColor+Extension.swift
@@ -19,18 +19,20 @@ extension UIColor {
 
     /// gray scale
     static let gray10 = UIColor(named: "albaid_gray_10")
-    static let gray20 = UIColor(named: "albaid_gray20")
-    static let gray30 = UIColor(named: "albaid_gray30")
-    static let gray40 = UIColor(named: "albaid_gray40")
-    static let gray50 = UIColor(named: "albaid_gray50")
-    static let gray60 = UIColor(named: "albaid_gray60")
-    static let gray70 = UIColor(named: "albaid_gray70")
-    static let gray80 = UIColor(named: "albaid_gray80")
-    static let gray90 = UIColor(named: "albaid_gray90")
-    static let gray95 = UIColor(named: "albaid_gray95")
-    static let gray100 = UIColor(named: "albaid_gray100")
+    static let gray20 = UIColor(named: "albaid_gray_20")
+    static let gray30 = UIColor(named: "albaid_gray_30")
+    static let gray40 = UIColor(named: "albaid_gray_40")
+    static let gray50 = UIColor(named: "albaid_gray_50")
+    static let gray60 = UIColor(named: "albaid_gray_60")
+    static let gray70 = UIColor(named: "albaid_gray_70")
+    static let gray80 = UIColor(named: "albaid_gray_80")
+    static let gray90 = UIColor(named: "albaid_gray_90")
+    static let gray95 = UIColor(named: "albaid_gray_95")
+    static let gray100 = UIColor(named: "albaid_gray_100")
 
     /// login
-    static let naver = UIColor(named: "login_naver")
-    static let kakao = UIColor(named: "login_kakao")
+    static let naver = UIColor(named: "albaid_login_naver")
+    static let kakao = UIColor(named: "albaid_login_kakao")
+
+    static let navy = UIColor(named: "albaid_navy")
 }
