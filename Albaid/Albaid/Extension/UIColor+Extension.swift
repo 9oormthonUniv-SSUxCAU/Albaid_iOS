84c0c8b5b4407b7f292d1b0dea790e84d4ffe7c8
Bibi-urssu <bibi.urssu@gmail.com>
2024-09-27 18:05:20 +0900

    feat: UIColor+Extension

--- Albaid/Albaid/Extension/UIColor+Extension.swift
+++ Albaid/Albaid/Extension/UIColor+Extension.swift
@@ -10,23 +10,27 @@ import UIKit
 extension UIColor {
 
     /// main color
-    static let mainGreen = UIColor(named: "main_green")
+    static let mainGreen = UIColor(named: "albaid_main_green")
 
     /// sub color
-    static let subYellow = UIColor(named: "sub_yellow")
-    static let subBlue = UIColor(named: "sub_blue")
-    static let subPink = UIColor(named: "sub_pink")
+    static let subYellow = UIColor(named: "albaid_sub_yellow")
+    static let subBlue = UIColor(named: "albaid_sub_blue")
+    static let subPink = UIColor(named: "albaid_sub_pink")
 
     /// gray scale
-    static let gray10 = UIColor(named: "gray10")
-    static let gray20 = UIColor(named: "gray20")
-    static let gray30 = UIColor(named: "gray30")
-    static let gray40 = UIColor(named: "gray40")
-    static let gray50 = UIColor(named: "gray50")
-    static let gray60 = UIColor(named: "gray60")
-    static let gray70 = UIColor(named: "gray70")
-    static let gray80 = UIColor(named: "gray80")
-    static let gray90 = UIColor(named: "gray90")
-    static let gray95 = UIColor(named: "gray95")
-    static let gray100 = UIColor(named: "gray100")
+    static let gray10 = UIColor(named: "albaid_gray_10")
+    static let gray20 = UIColor(named: "albaid_gray20")
+    static let gray30 = UIColor(named: "albaid_gray30")
+    static let gray40 = UIColor(named: "albaid_gray40")
+    static let gray50 = UIColor(named: "albaid_gray50")
+    static let gray60 = UIColor(named: "albaid_gray60")
+    static let gray70 = UIColor(named: "albaid_gray70")
+    static let gray80 = UIColor(named: "albaid_gray80")
+    static let gray90 = UIColor(named: "albaid_gray90")
+    static let gray95 = UIColor(named: "albaid_gray95")
+    static let gray100 = UIColor(named: "albaid_gray100")
+
+    /// login
+    static let naver = UIColor(named: "login_naver")
+    static let kakao = UIColor(named: "login_kakao")
 }
