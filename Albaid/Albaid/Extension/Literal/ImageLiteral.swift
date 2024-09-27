97404c0eda0bd6e09fdb7522b0517d58e7f70749
Bibi-urssu <bibi.urssu@gmail.com>
2024-09-27 15:15:06 +0900

    add: asset 및 ImageLiteral 추가

---
+++ Albaid/Albaid/Extension/Literal/ImageLiteral.swift
@@ -0,0 +1,62 @@
+//
+//  ImageLiteral.swift
+//  Albaid
+//
+//  Created by 박지윤 on 9/27/24.
+//
+
+import UIKit
+
+public enum AlbaidImage {
+    public static var splash: UIImage { .load(name: "splash")}
+    public static var logo: UIImage { .load(name: "logo")}
+
+    /// onboarding
+    public static var onboarding1: UIImage { .load(name: "onboarding1")}
+    public static var onboarding2: UIImage { .load(name: "onboarding2")}
+    public static var onboarding3: UIImage { .load(name: "onboarding3")}
+}
+
+public enum AlbaidButton {
+    /// tabBar
+    public static var home: UIImage { .load(name: "ic_tabbar_home") }
+    public static var homeFilled: UIImage { .load(name: "ic_tabbar_home_filled") }
+    public static var calendar: UIImage { .load(name: "ic_tabbar_calendar") }
+    public static var calendarFilled: UIImage { .load(name: "ic_tabbar_calendar_filled") }
+    public static var card: UIImage { .load(name: "ic_tabbar_card") }
+    public static var cardFilled: UIImage { .load(name: "ic_tabbar_card_filled") }
+    public static var myPage: UIImage { .load(name: "ic_tabbar_mypage") }
+    public static var myPageFilled: UIImage { .load(name: "ic_tabbar_mypage_filled") }
+
+    /// navigation
+    public static var back: UIImage { .load(name: "ic_navigation_back") }
+    public static var close: UIImage { .load(name: "ic_navigation_close") }
+    public static var bell: UIImage { .load(name: "ic_navigation_bell") }
+    public static var menu: UIImage { .load(name: "ic_navigation_menu") }
+
+    /// menu
+    public static var share: UIImage { .load(name: "ic_menu_share") }
+    public static var edit: UIImage { .load(name: "ic_menu_edit") }
+    public static var delete: UIImage { .load(name: "ic_menu_delete") }
+
+    /// home
+    public static var camera: UIImage { .load(name: "ic_home_camera") }
+    public static var contract: UIImage { .load(name: "ic_home_contract") }
+    public static var resume: UIImage { .load(name: "ic_home_resemu") }
+
+    /// etc
+    public static var detail: UIImage { .load(name: "ic_detail") }
+}
+
+public enum AlabidIcon {
+}
+
+extension UIImage {
+    static func load(name: String) -> UIImage {
+        guard let image = UIImage(named: name, in: nil, compatibleWith: nil) else {
+            return UIImage()
+        }
+        image.accessibilityIdentifier = name
+        return image
+    }
+}
