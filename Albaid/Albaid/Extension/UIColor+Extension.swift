0baa1e3ac63488c833295bb9a8f37d99a885d097
Bibi-urssu <bibi.urssu@gmail.com>
2024-09-27 17:36:15 +0900

    add: UIColor+Extension
    gray scale color

--- Albaid/Albaid/Extension/UIColor+Extension.swift
+++ Albaid/Albaid/Extension/UIColor+Extension.swift
@@ -10,10 +10,23 @@ import UIKit
 extension UIColor {
 
     /// main color
-    static let mainGreen = UIColor(named: "green")
+    static let mainGreen = UIColor(named: "main_green")
 
     /// sub color
-    static let subYellow = UIColor(named: "yellow")
-    static let subBlue = UIColor(named: "blue")
-    static let subPink = UIColor(named: "pink")
+    static let subYellow = UIColor(named: "sub_yellow")
+    static let subBlue = UIColor(named: "sub_blue")
+    static let subPink = UIColor(named: "sub_pink")
+
+    /// gray scale
+    static let gray10 = UIColor(named: "gray10")
+    static let gray20 = UIColor(named: "gray20")
+    static let gray30 = UIColor(named: "gray30")
+    static let gray40 = UIColor(named: "gray40")
+    static let gray50 = UIColor(named: "gray50")
+    static let gray60 = UIColor(named: "gray60")
+    static let gray70 = UIColor(named: "gray70")
+    static let gray80 = UIColor(named: "gray80")
+    static let gray90 = UIColor(named: "gray90")
+    static let gray95 = UIColor(named: "gray95")
+    static let gray100 = UIColor(named: "gray100")
 }
