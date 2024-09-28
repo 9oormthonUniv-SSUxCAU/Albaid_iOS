616410a35c7b8aaf3b011149d01742cf4e67562e
Bibi-urssu <bibi.urssu@gmail.com>
2024-09-28 21:59:08 +0900

    feat: UIButton+Extension setMenuButton 구현

--- Albaid/Albaid/Extension/UIButton+Extension.swift
+++ Albaid/Albaid/Extension/UIButton+Extension.swift
@@ -10,13 +10,18 @@ import UIKit.UIButton
 extension UIButton {
     func setLoginButton(image: UIImage, title: String, titleColor: UIColor, backgroundColor: UIColor) {
         setDefaultButton()
+        
+        var attributedTitle = AttributedString(title)
+        attributedTitle.font = UIFont(name: "Pretendard-SemiBold", size: 15.0)
+        attributedTitle.foregroundColor = titleColor
 
-        setImage(image, for: .normal)
-        setTitle(title, for: .normal)
-        setTitleColor(titleColor, for: .normal)
+        var configuration = UIButton.Configuration.plain()
+        configuration.attributedTitle = attributedTitle
+        configuration.image = image
+        configuration.imagePadding = 15
+        configuration.imagePlacement = .leading
+        self.configuration = configuration
         self.backgroundColor = backgroundColor
-        // TODO: 
-        imageEdgeInsets = .init(top: 0, left: -15, bottom: 0, right: 0)
     }
 
     func setDefaultButton() {
@@ -25,5 +30,28 @@ extension UIButton {
             $0.height.equalTo(54)
         }
         titleLabel?.font = UIFont(name: "Pretendard-SemiBold", size: 16.0)
+    }
+
+    func setMenuButton(image: UIImage, title: String) {
+        var attributedTitle = AttributedString(title)
+        attributedTitle.font = UIFont(name: "Pretendard-SemiBold", size: 15.0)
+        attributedTitle.foregroundColor = .gray30
+
+        var configuration = UIButton.Configuration.plain()
+        configuration.attributedTitle = attributedTitle
+        configuration.image = image
+        configuration.imagePadding = 12
+        configuration.imagePlacement = .top
+        configuration.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)
+        self.configuration = configuration
+
+        snp.makeConstraints {
+            $0.height.width.equalTo(70)
+        }
+    }
+
+    func setBorder() {
+        layer.borderWidth = 1
+        layer.borderColor = UIColor.gray80?.cgColor
     }
 }
