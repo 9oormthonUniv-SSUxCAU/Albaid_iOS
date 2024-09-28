ce32fbaf20430b7eee6219bf8f45c7274df9b91d
Bibi-urssu <bibi.urssu@gmail.com>
2024-09-28 22:40:30 +0900

    feat: Int+Extension

---
+++ Albaid/Albaid/Extension/Int+Extension.swift
@@ -0,0 +1,20 @@
+//
+//  Int+Extension.swift
+//  Albaid
+//
+//  Created by 박지윤 on 9/28/24.
+//
+
+import Foundation
+
+extension Int {
+    var toPriceFormat: String {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        return formatter.string(from: NSNumber(value: self)) ?? ""
+    }
+
+    var toMonthFormat: String {
+        return "\(self)월"
+    }
+}
